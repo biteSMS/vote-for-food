8171e66041205a6a22c319b18e56d7458d8fe032
bibi <348856022@qq.com>
2018-11-09 21:31:02 +0800

    项目初始化

---
+++ src/App/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './normalize.css'
+import './index.scss'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>hello测试一下，。！@¥  1234afsd:@!$</div>
+        )
+    }
+}
